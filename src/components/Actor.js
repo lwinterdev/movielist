7dee081a3b4fb511aa4d4695fa8da9e2f4966766
lwinterdev <101981389+lwinterdev@users.noreply.github.com>
2022-04-26 21:03:51 +0200

    updated design for actor and movie

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Actor = ({name,character,imageURL}) => {
   return (
-    <div className='col p-2'>
+    <div className='col p-2 bg-secondary border border-info m-3 '>
        
-        <h3>{name}</h3>
+        <h3 className=''>{name}</h3>
         <p>{character}</p>
 
     </div>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -23,63 +23,61 @@ const Movie = () => {
     
     return (
     <div className='bg-dark text-light container-fluid'>
-        
+        <div>
         <div style={{'maxWidth':'400px'}} className='row'>
             <img src={movie.poster_path && IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path } alt="movie-poster"/>
         </div>
         
         
         <div className='col'>
-        <h1 className='col'>{movie.title}</h1>
-        <div className='col'>{movie.tagline}</div>
-        
-        
-        {movie.runtime > 0 && <div>
-            <h3>Runtime</h3>
-            <p className='col'>{movie.runtime} Minutes</p>
-            </div> }
-        
-        
-        <h3 className='col p-1'>Plot</h3>
-        <p className='col' >{movie.overview}</p>
-        
-        
-        <h3 className='col'>Directing</h3>
-        {movie.directors && movie.directors.map(director => (
-            <div key={director.id }className='p-2'>{director.name}</div>
-        ))}
-        
-
-        <h3 className='col'>Released</h3>{movie.release_date ?
-            <p className='col'>{movie.release_date}</p>
-                :
-            <p>Not Yet Released</p>}
-        
-
-        <h3 className='col'>Actors</h3>
-        <div className='row'>
+           
             
-        
-        {movie.actors && movie.actors.map(actor => (
-          
-          <Actor
-            key={actor.credit_id}
-            name={actor.name}
-            character={actor.character}
-            imageUrl={
-              actor.profile_path
-                && `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+            {movie.runtime > 0 && <div>
+                <h3 className='text-info'>Runtime</h3>
+                <p className='col '>{movie.runtime} Minutes</p>
+                </div> }
+            
+            
+            <h3 className='col p-1 text-info'>Plot</h3>
+            <p className='col ' >{movie.overview}</p>
+            
+            
+            <h3 className='col text-info'>Directing</h3>
+            {movie.directors && movie.directors.map(director => (
+                <div key={director.id }className='p-2 '>{director.name}</div>
+            ))}
+            
+
+            <h3 className='col text-info'>Released</h3>{movie.release_date ?
+                <p className='col'>{movie.release_date}</p>
+                    :
+                <p>Not Yet Released</p>}
+            
+
+            <h3 className='col text-info'>Actors</h3>
+            <div className='row'>
                 
-            }
-          />
-        ))}
-        </div>
-  
-        
-        <h3>Rating</h3>
-        <div>{movie.vote_average}</div>
-        </div>
-        
+            
+            {movie.actors && movie.actors.map(actor => (
+            
+            <Actor
+                key={actor.credit_id}
+                name={actor.name}
+                character={actor.character}
+                imageUrl={
+                actor.profile_path
+                    && `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                    
+                }
+            />
+            ))}
+            </div>
+    
+            
+            <h3 className='text-info'>Rating</h3>
+            <div className='bg-dark'>{movie.vote_average}</div>
+            </div>
+            </div>
     </div>
   )
 }
